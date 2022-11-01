2903a71c922b3db418db5121d8c98822d627d2eb
Ryan See <ryanjosephsee@gmail.com>
2022-11-01 15:52:39 -0700

    commit tree 8805057119046c7ccdecb6791265e80964f87dc3

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+function superbowlWin(games) {
+    const wonGame = games.find((game) => {
+        return game.result === "W" 
+    
+    });
+    wonGames.map(game => {
+        return wonGame.year;
+    })
+}
